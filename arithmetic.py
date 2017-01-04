8992f3c344cdb61013b9d999475de1354427eb78
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-04 12:01:12 -0800

    Initial commit

---
+++ arithmetic.py
@@ -0,0 +1,31 @@
+def add(num1, num2):
+    """Return the sum of two numbers"""
+    return 10
+
+def subtract(num1, num2):
+    """Return the difference of two numbers"""
+    pass
+
+def multiply(num1, num2):
+    """Return the product of two numbers"""
+    pass
+
+def divide(num1, num2):
+    """Return the quotient of two numbers as a float"""
+    pass
+
+def square(num):
+    """Return the square of a number"""
+    pass
+
+def cube(num):
+    """Return the cube of a number"""
+    pass
+
+def power(num, exponent):
+    """Return num raised to the power of exponent"""
+    pass
+
+def mod(num1, num2):
+    """Return remainder of num1 divided by num2"""
+    pass
